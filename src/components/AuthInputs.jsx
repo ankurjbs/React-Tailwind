29d3c29c360d1e26ce1032d613a0a8ae7f645bb5
Ankur Maurya <31955983+ankurjbs@users.noreply.github.com>
2024-05-25 21:29:54 +0530

    Initial commit

---
+++ src/components/AuthInputs.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import Input from './Input';
+import DivContainer from './Div';
+
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value) {
+    if (identifier === 'email') {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  return (
+    <div id="auth-inputs">
+      <DivContainer>
+          <Input label="Email"
+            type="email"
+            invalid={emailNotValid}
+            onChange={(event) => handleInputChange('email', event.target.value)}
+          />        
+          <Input label="Password"
+            type="password"
+            invalid = {passwordNotValid}
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+          />
+        
+        </DivContainer>
+      <div className="actions">
+        <button type="button" className="text-button">
+          Create a new account
+        </button>
+        <button className='button' onClick={handleLogin}>Sign In</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Div.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+const DivContainer = styled.div`
+  diplay : flex;
+  flex-direction : column;
+  gap : 0.5rem;
+  margin-bottom : 1.5rem;
+`
+
+export default DivContainer;
